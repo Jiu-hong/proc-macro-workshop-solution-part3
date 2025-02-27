cfa2f1095a97f710c91bc0d115768666c767f33a
Jiu-hong <jiu.hong.sun@gmail.com>
2025-02-27 21:24:56 +0800

    03ok

--- src/lib copy.rs
+++ src/lib copy.rs
@@ -2,7 +2,7 @@
 use std::str::FromStr;
 
 use proc_macro::TokenStream;
-use proc_macro2::Group;
+use proc_macro2::{Group, TokenTree};
 use quote::{ToTokens, quote, quote_spanned};
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
@@ -31,17 +31,13 @@ struct SeqStruct {
 impl Parse for SeqStruct {
     fn parse(input: ParseStream) -> Result<Self> {
         let name: Ident = input.parse()?;
-        eprintln!("name is {}", name);
         input.parse::<Token![in]>()?;
         let from: syn::LitInt = input.parse()?;
         input.parse::<Token![..]>()?;
         let to: syn::LitInt = input.parse()?;
-        eprintln!("to is {}", to);
         let content;
-        let brace_token = syn::braced!(content in input);
-        eprintln!("content is {}", content);
+        let _brace_token = syn::braced!(content in input);
         let inner: proc_macro2::TokenStream = content.parse()?;
-        eprintln!("inner is {}", inner);
         Ok(SeqStruct {
             name,
             from,
@@ -51,67 +47,61 @@ impl Parse for SeqStruct {
     }
 }
 
-fn get_group_ident(group: &Group) {
-    let a = group.stream();
-    if a.to_string() == "N" {
-        let new_token = proc_macro2::TokenStream::from_str(&x.to_string()).unwrap();
-        // replace group's name with specific number
-        let d = group.delimiter();
-        let new_group = Group::new(d, new_token.clone());
-        token = proc_macro2::TokenTree::Group(new_group);
-    }
-    //final symbol
-    //recursive
-    else {
-        for token in a {
-            // a.into_iter().map(|mut token| {
-            if let proc_macro2::TokenTree::Group(ref group) = token {
-                get_group_ident(group)
+fn update_group_ident(
+    tokenstream: proc_macro2::TokenStream,
+    name: &Ident,
+    index: u64,
+) -> proc_macro2::TokenStream {
+    // for current_token in tokenstream {
+    tokenstream
+        .into_iter()
+        .map(|current_token| {
+            // if current_token is Group {
+            if let proc_macro2::TokenTree::Group(ref group) = current_token {
+                let delimiter = group.delimiter();
+                // if current_token'stream length is 1 {
+                if group.stream().into_iter().count() == 1 {
+                    // if current_token == "N"
+                    if group.stream().to_string() == name.to_string() {
+                        //  {current_token = customized_token //change current token.}
+                        let new_token =
+                            proc_macro2::TokenStream::from_str(&index.to_string()).unwrap();
+                        // replace group's name with specific number
+                        let new_group = Group::new(delimiter, new_token.clone());
+                        let customized_token = proc_macro2::TokenTree::Group(new_group);
+                        return customized_token;
+                    } else {
+                        return current_token;
+                    }
+                } else {
+                    let inner_tokenstream = update_group_ident(group.stream(), name, index);
+                    let new_group = Group::new(delimiter, inner_tokenstream.clone());
+                    let customized_token = proc_macro2::TokenTree::Group(new_group);
+                    return customized_token;
+                }
             }
-        }
-    }
+            return current_token;
+        })
+        .collect::<proc_macro2::TokenStream>()
 }
-// [x*2 for x in range(5)]
+
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as SeqStruct);
-    println!("input is {:#?}", input);
     let name = input.name;
     let from = input.from;
-    let from_token_stream = from.to_token_stream();
     let from_int: u64 = from.base10_parse().unwrap();
     let to = input.to;
-    let to_token_stream = to.to_token_stream();
     let to_int: u64 = to.base10_parse().unwrap();
-    let mut inner = input.inner;
+    let inner = input.inner;
+    eprintln!("inner is {:#?}", inner);
 
-    // let result: Vec<TokenStream> = new_inner_iter.collect();
-    let output = (from_int..to_int).map(|x| {
-        let new_inner_iter = inner.clone().into_iter().map(|mut token| {
-            eprintln!("token is {:#?}", token);
-            if let proc_macro2::TokenTree::Group(ref group) = token {
-                get_group_ident(group);
-                let a: proc_macro2::TokenStream = group.stream();
-                eprintln!("a is {:#?}", a.to_string());
-                if a.to_string() == name.to_string() {
-                    let new_token = proc_macro2::TokenStream::from_str(&x.to_string()).unwrap();
-                    // replace group's name with specific number
-                    let d = group.delimiter();
-                    let new_group = Group::new(d, new_token.clone());
-                    token = proc_macro2::TokenTree::Group(new_group);
-                }
-            }
-            return token;
-        });
-        let new_inner = proc_macro2::TokenStream::from_iter(new_inner_iter);
-        eprintln!("new_inner: {:#?}", new_inner);
-        println!("number is {}", x);
-        quote! {#new_inner}
-    });
+    let output = (from_int..to_int)
+        .map(|x| {
+            let new_inner = update_group_ident(inner.clone(), &name, x);
+            quote! {#new_inner}
+        })
+        .collect::<proc_macro2::TokenStream>();
 
-    // eprintln!("output is {}", output);
-    let a = output.collect::<proc_macro2::TokenStream>();
-    // let a = inner.parse()?;
-    eprintln!("output is: {:#?}", a);
-    a.into()
+    output.into()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@
 use std::str::FromStr;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Group, TokenTree};
-use quote::{ToTokens, quote, quote_spanned};
+use proc_macro2::{Delimiter, Group, Literal, Punct, Span, TokenTree};
+use quote::{ToTokens, format_ident, quote, quote_spanned};
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
+use syn::token::{Brace, RArrow};
 use syn::{Expr, Ident, Token, Type, Visibility, parse_macro_input, token};
 // use syn::{Field, Ident, ItemEnum, Result, Token, braced, token};
 
@@ -22,12 +23,27 @@ struct SeqStruct {
     name: Ident,
     from: syn::LitInt,
     to: syn::LitInt,
-    inner: proc_macro2::TokenStream,
+    // inner: proc_macro2::TokenStream,
+    fn_name: Token![fn],
+    f_name: Ident,
+    n_ident: Ident,
+    group: Group,
+    return_arrow: RArrow,
+    return_type: Ident,
+    fn_brace_token: Brace,
+    inner_ident: Ident,
+    inner_punct: Punct,
+    inner_literal: Literal,
 }
 // seq!(N in 0..8 {
 //     // nothing
 // });
 
+// seq!(N in 1..4 {
+//     fn f~N () -> u64 {
+//         N * 2
+//     }
+// });
 impl Parse for SeqStruct {
     fn parse(input: ParseStream) -> Result<Self> {
         let name: Ident = input.parse()?;
@@ -37,70 +53,125 @@ impl Parse for SeqStruct {
         let to: syn::LitInt = input.parse()?;
         let content;
         let _brace_token = syn::braced!(content in input);
-        let inner: proc_macro2::TokenStream = content.parse()?;
+        let fn_name = content.parse::<Token![fn]>()?;
+        let f_name: Ident = content.parse()?;
+        content.parse::<Token![~]>()?;
+        let n_ident: Ident = content.parse()?;
+        let group: Group = content.parse()?;
+        let return_arrow = content.parse::<Token![->]>()?;
+
+        let return_type: Ident = content.parse()?;
+        let content_inner;
+        let fn_brace_token = syn::braced!(content_inner in content);
+        let inner_ident: Ident = content_inner.parse()?;
+        let inner_punct: Punct = content_inner.parse()?;
+        let inner_literal: Literal = content_inner.parse()?;
+
+        // let inner: proc_macro2::TokenStream = content.parse()?;
         Ok(SeqStruct {
             name,
             from,
             to,
-            inner,
+            fn_name,
+            f_name,
+            n_ident,
+            group,
+            return_arrow,
+            return_type,
+            fn_brace_token,
+            inner_ident,
+            inner_punct,
+            inner_literal,
         })
     }
 }
 
-fn update_group_ident(
-    tokenstream: proc_macro2::TokenStream,
-    name: &Ident,
-    index: u64,
-) -> proc_macro2::TokenStream {
-    // for current_token in tokenstream {
-    tokenstream
-        .into_iter()
-        .map(|current_token| {
-            // if current_token is Group {
-            if let proc_macro2::TokenTree::Group(ref group) = current_token {
-                let delimiter = group.delimiter();
-                // if current_token'stream length is 1 {
-                if group.stream().into_iter().count() == 1 {
-                    // if current_token == "N"
-                    if group.stream().to_string() == name.to_string() {
-                        //  {current_token = customized_token //change current token.}
-                        let new_token =
-                            proc_macro2::TokenStream::from_str(&index.to_string()).unwrap();
-                        // replace group's name with specific number
-                        let new_group = Group::new(delimiter, new_token.clone());
-                        let customized_token = proc_macro2::TokenTree::Group(new_group);
-                        return customized_token;
-                    } else {
-                        return current_token;
-                    }
-                } else {
-                    let inner_tokenstream = update_group_ident(group.stream(), name, index);
-                    let new_group = Group::new(delimiter, inner_tokenstream.clone());
-                    let customized_token = proc_macro2::TokenTree::Group(new_group);
-                    return customized_token;
-                }
-            }
-            return current_token;
-        })
-        .collect::<proc_macro2::TokenStream>()
-}
-
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
+    eprintln!("input is {:#?}", input);
     let input = parse_macro_input!(input as SeqStruct);
     let name = input.name;
     let from = input.from;
     let from_int: u64 = from.base10_parse().unwrap();
     let to = input.to;
     let to_int: u64 = to.base10_parse().unwrap();
-    let inner = input.inner;
+    // fn_name: Ident,
+    let fn_name = input.fn_name;
+    // f_name: Ident,
+    let f_name = input.f_name;
+    // n_ident: Ident,
+    let n_ident = input.n_ident;
+    // group: Group,
+    let group = input.group;
+    // punct_: Punct,
+    let return_arrow = input.return_arrow;
+    // return_type: Ident,
+    let return_type = input.return_type;
+    // fn_brace_token: Brace,
+    let fn_brace_token = input.fn_brace_token;
+    // inner_ident: Ident,
+    let inner_ident = input.inner_ident;
+    // inner_punct: Punct,
+    let inner_punct = input.inner_punct;
+    // inner_literal: Literal,
+    let inner_literal = input.inner_literal;
+
+    // eprintln!("inner is {:#?}", inner);
+    eprintln!("name is {:#?}", name);
+    eprintln!("from is {:#?}", from);
+    eprintln!("to is {:#?}", to);
+    eprintln!("fn_name is {:#?}", fn_name);
+    eprintln!("f_name is {:#?}", f_name);
+    eprintln!("n_ident is {:#?}", n_ident);
+    eprintln!("group is {:#?}", group);
+    eprintln!("return_arrow {:#?}", return_arrow);
+    eprintln!("return_type is {:#?}", return_type);
+    eprintln!("fn_brace_token is {:#?}", fn_brace_token);
+    eprintln!("inner_ident is {:#?}", inner_ident);
+    eprintln!("inner_punct is {:#?}", inner_punct);
+    eprintln!("inner_literal is {:#?}", inner_literal);
+
+    let fn_token_stream = fn_name.to_token_stream();
+    eprintln!("fn_token_stream is {:#?}", fn_token_stream);
+
+    let group_token = group.to_token_stream();
+    let return_arrow_token: proc_macro2::TokenStream = return_arrow.to_token_stream();
+    let return_type_token = return_type.to_token_stream();
 
     let output = (from_int..to_int)
-        .map(|x| {
-            let new_inner = update_group_ident(inner.clone(), &name, x);
-            quote! {#new_inner}
+        .map(|index| {
+            // let f_name_ident = f_name;
+            let fn_name = f_name.to_string() + &index.to_string();
+            let f_name_ident = Ident::new(&fn_name.to_string(), f_name.span());
+
+            // inner group
+            let inner_ident_token = index.to_token_stream();
+            let inner_punct_token = inner_punct.to_token_stream();
+            let inner_literal_token = inner_literal.to_token_stream();
+            let inner_group = Group::new(
+                Delimiter::Brace,
+                proc_macro2::TokenStream::from_iter(
+                    [inner_ident_token, inner_punct_token, inner_literal_token].into_iter(),
+                ),
+            );
+
+            let output_each: proc_macro2::TokenStream = proc_macro2::TokenStream::from_iter(
+                [
+                    fn_token_stream.clone(),
+                    // f_token_stream.clone(), add number
+                    f_name_ident.to_token_stream(),
+                    group_token.clone(),
+                    return_arrow_token.clone(),
+                    return_type_token.clone(),
+                    inner_group.into_token_stream(),
+                ]
+                .into_iter(),
+            );
+            quote! {#output_each}
         })
         .collect::<proc_macro2::TokenStream>();
+    // eprintln!("output is {}", output_each);
 
+    // quote! {#output_each}.into()
     output.into()
 }
